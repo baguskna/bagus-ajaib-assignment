e245df4cb5c5e7ff900d1f0a2f850d0ac2619e3c
Bagus Kurnia <bagus@alore.io>
2022-05-20 21:02:32 +0700

    Add home unit test

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+  it("should render title in header", () => {
+    render(<Home />);
+    const headingElement = screen.getByText(/Example With Search and Filter/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+});
