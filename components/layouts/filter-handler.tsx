4c4b60cbfe948d5309ac7dae2e38bdd66ca78723
Bagus Kurnia <bagus@alore.io>
2022-05-21 16:35:35 +0700

    Add reset filter button

--- components/layouts/filter-handler.tsx
+++ components/layouts/filter-handler.tsx
@@ -1,11 +1,11 @@
 import { css, jsx } from "@emotion/react";
 import { EmotionJSX } from "@emotion/react/types/jsx-namespace";
 import { useState } from "react";
-import { useEffect } from "react";
 import { useCallback } from "react";
-import { bp } from "../../lib/breakpoints";
+import CloseIcon from "@mui/icons-material/Close";
+import { bp, bpOnly } from "../../lib/breakpoints";
 import { COLORS } from "../../lib/colors";
-import { LABEL } from "../../lib/design-system";
+import { BUTTON, LABEL } from "../../lib/design-system";
 
 const filterHandlerContainerStyles = css`
   display: flex;
@@ -21,53 +21,169 @@ const inputWrapperStyles = css`
   border: 1px solid ${COLORS.grey_100};
   border-radius: 5px;
   display: flex;
+  margin-top: 4px;
+  min-width: 200px;
 `;
 
 const inputStyles = css`
-  border: 0;
-  border-radius: 5px;
-  padding: 5px 8px;
+  border: 2px solid transparent;
+  border-bottom-left-radius: 5px;
+  border-top-left-radius: 5px;
+  height: 22px;
+  padding: 8px 12px;
   width: 100%;
+
+  &:focus {
+    border: 2px solid ${COLORS.blue_100};
+  }
+
+  &:focus-visible {
+    outline: transparent;
+  }
+`;
+
+const buttonClearStyles = css`
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  justify-content: center;
+  padding: 0 10px;
+`;
+
+const closeIconStyles = css`
+  color: ${COLORS.grey_500};
+  cursor: pointer;
+  opacity: 0.7;
+  width: 15px;
+`;
+
+const labelGenderStyles = css`
+  display: flex;
+  flex-direction: column;
+
+  ${bpOnly[0]} {
+    margin-top: 8px;
+  }
+
+  ${bp[0]} {
+    margin-left: 16px;
+  }
+`;
+
+const selectGenderStyles = css`
+  appearance: none;
+  background-position: right 8px center;
+  background-repeat: no-repeat;
+  background-size: 24px 24px;
+  border: 1px solid ${COLORS.grey_100};
+  border-radius: 5px;
+  padding: 0 12px;
+  height: 44px;
+  margin-top: 4px;
+  min-width: 200px;
+
+  &:focus,
+  &:focus-visible {
+    outline: 2px solid ${COLORS.blue_100};
+  }
+`;
+
+const buttonResetStyles = css`
+  height: 45px;
+  margin-left: 16px;
+  margin-top: auto;
+
+  &:hover {
+    background-color: ${COLORS.blue_200};
+  }
 `;
 
 const FilterHandler: () => EmotionJSX.Element = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [genderValue, setGenderValue] = useState<string>("");
 
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputValue(e.target.value);
-    },
-    [setInputValue]
-  );
+  const onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void =
+    useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+      },
+      [setInputValue]
+    );
 
-  const handleClear = () => {
+  const onGenderValueChange: (e: React.ChangeEvent<HTMLSelectElement>) => void =
+    useCallback(
+      (e: React.ChangeEvent<HTMLSelectElement>) => {
+        if (!e.target.value) {
+          setGenderValue("");
+          return;
+        }
+        setGenderValue(e.target.value);
+      },
+      [setGenderValue]
+    );
+
+  const onClearClick: () => void = () => {
     setInputValue("");
+  };
+
+  const onResetFilterClick: () => void = () => {
+    setGenderValue("");
   };
 
   return (
     <div css={filterHandlerContainerStyles}>
-      <LABEL htmlFor="keyword">Search</LABEL>
-      <div css={inputWrapperStyles}>
-        <input
-          css={inputStyles}
-          type="text"
-          name="keyword"
-          id="keyword"
-          autoComplete="off"
-          placeholder="Search..."
-          value={inputValue}
-          onChange={handleChange}
-        />
-        <button type="button" data-testid="close-button" onClick={handleClear}>
-          X
-        </button>
+      <div>
+        <LABEL htmlFor="keyword">Search</LABEL>
+        <div css={inputWrapperStyles}>
+          <input
+            css={inputStyles}
+            type="text"
+            name="keyword"
+            id="keyword"
+            autoComplete="off"
+            placeholder="Search..."
+            value={inputValue}
+            onChange={onInputChange}
+          />
+          <button
+            css={buttonClearStyles}
+            type="button"
+            data-testid="close-button"
+            onClick={onClearClick}
+          >
+            <CloseIcon css={closeIconStyles} />
+          </button>
+        </div>
       </div>
-      <LABEL htmlFor="gender">Gender</LABEL>
-      <select id="gender" name="gender">
-        <option value="">All</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-      </select>
+      <div css={labelGenderStyles}>
+        <LABEL htmlFor="gender">Gender</LABEL>
+        <select
+          css={selectGenderStyles}
+          id="gender"
+          name="gender"
+          data-testid="gender"
+          value={genderValue}
+          onChange={onGenderValueChange}
+        >
+          <option value="">All</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <BUTTON
+          css={buttonResetStyles}
+          type="button"
+          onClick={onResetFilterClick}
+        >
+          Reset Filter
+        </BUTTON>
+      </div>
     </div>
   );
 };
