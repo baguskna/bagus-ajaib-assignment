9fd5fc8e583503a2612e407008ccd8df18bb7794
Bagus Kurnia <bagus@alore.io>
2022-05-21 10:44:02 +0700

    Add filter handler text input and select tag html

--- __test__/filter-handler.test.tsx
+++ __test__/filter-handler.test.tsx
@@ -10,7 +10,19 @@ describe("Filter Handler component", () => {
 
   it("should render gender in filter handler", () => {
     render(<FilterHandler />);
-    const searchElement = screen.getByText(/gender/i);
-    expect(searchElement).toBeInTheDocument();
+    const genderElement = screen.getByText(/gender/i);
+    expect(genderElement).toBeInTheDocument();
+  });
+
+  it("should render text input", () => {
+    render(<FilterHandler />);
+    const inputElement = screen.getByPlaceholderText(/search/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should render select input value 'all'", () => {
+    render(<FilterHandler />);
+    const selectElement = screen.getByDisplayValue(/all/i);
+    expect(selectElement).toBeInTheDocument();
   });
 });
